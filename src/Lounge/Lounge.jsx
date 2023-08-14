749962cb71ce0806c9f9eb602f870da6bbb95d63
qefiney <qefiney@gmail.com>
2023-08-14 07:44:51 +0300

    Lounge section jsx layout

--- src/Lounge/Lounge.jsx
+++ src/Lounge/Lounge.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import imageGrid from "../assets/images-grid.png";
 
 const Lounge = () => {
-	return <div>Lounge</div>;
+	return (
+		<div className="lounge container section">
+			<div className="setContainer grid">
+
+				<div className="imgDiv">
+					<img src={imageGrid} />
+				</div>
+
+				<div className="textDiv">
+					<h2>Unaccompanied Minor Lounge</h2>
+				</div>
+
+				<div className="grids grid">
+
+					<div className="singleGrid">
+						<span className="gridTitle">
+							Help through the airport
+						</span>
+						<p>
+							Yu can also call airlines from your phone and book a flight ticket to one of you favorite destinations.
+						</p>
+					</div>
+
+					<div className="singleGrid">
+						<span className="gridTitle">
+							Priority Boarding
+						</span>
+						<p>
+							Yu can also call airlines from your phone and book a flight ticket to one of you favorite destinations.
+						</p>
+					</div>
+
+					<div className="singleGrid">
+						<span className="gridTitle">
+							Care on Flight
+						</span>
+						<p>
+							Yu can also call airlines from your phone and book a flight ticket to one of you favorite destinations.
+						</p>
+					</div>
+
+					<div className="singleGrid">
+						<span className="gridTitle">
+							Chauffeur-drive services
+						</span>
+						<p>
+							Yu can also call airlines from your phone and book a flight ticket to one of you favorite destinations.
+						</p>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+	);
 };
 
 export default Lounge;
