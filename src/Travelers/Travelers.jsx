c724328b9c27812f34e797840f8516bad96c1d52
qefiney <qefiney@gmail.com>
2023-08-16 19:22:19 +0300

    Travelers layout update

--- src/Travelers/Travelers.jsx
+++ src/Travelers/Travelers.jsx
@@ -3,10 +3,12 @@
 import City1 from "../assets/c1.jpg";
 import City2 from "../assets/c2.jpg";
 import City3 from "../assets/c3.jpg";
+import City4 from "../assets/c4.jpg";
 // Imported Traveler Image ====>
 import Person1 from "../assets/pp1.jpg";
 import Person2 from "../assets/pp2.jpg";
 import Person3 from "../assets/pp3.jpg";
+import Person4 from "../assets/pp4.jpg";
 
 // Displaying data using map array method ====>
 const travelers = [
@@ -17,17 +19,24 @@ const travelers = [
 		travelerName: "Wefiney",
 		socialLink: "@wefiney",
 	},
-  {
+	{
 		id: 2,
 		destinationImage: City2,
 		travelerImage: Person2,
 		travelerName: "Wefiney",
 		socialLink: "@wefiney",
 	},
-  {
+	{
 		id: 3,
 		destinationImage: City3,
 		travelerImage: Person3,
+		travelerName: "Wefiney",
+		socialLink: "@wefiney",
+	},
+	{
+		id: 4,
+		destinationImage: City4,
+		travelerImage: Person4,
 		travelerName: "Wefiney",
 		socialLink: "@wefiney",
 	},
@@ -41,7 +50,13 @@ const Travelers = () => {
 
 				<div className="travelersContainer grid">
 					{travelers.map(
-						({id, destinationImage, travelerImage, travelerName, socialLink}) => {
+						({
+							id,
+							destinationImage,
+							travelerImage,
+							travelerName,
+							socialLink,
+						}) => {
 							return (
 								<div key={id} className="singleTraveler">
 									<img
@@ -67,20 +82,6 @@ const Travelers = () => {
 							);
 						}
 					)}
-
-					{/* <div className="singleTraveler">
-						<img src={City1} alt="" className="destinationImage" />
-
-						<div className="travelerDetails">
-							<div className="travelerPicture">
-								<img src={Person1} alt="" className="travelerImage" />
-							</div>
-							<div className="travelerName">
-								<span>Wefiney</span>
-								<p>@Wefiney</p>
-							</div>
-						</div>
-					</div> */}
 				</div>
 			</div>
 		</div>
